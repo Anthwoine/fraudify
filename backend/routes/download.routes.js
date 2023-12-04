cdd13ccfc0125fe4af1f02c6db7886e8b184703b
Anthwoine <Anthwoine49@gmail.com>
2023-12-04 19:28:09 +0100

    ajout des routes

---
+++ backend/routes/download.routes.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send("Voici un message de l'API");
+});
+
+// app.get('/post', (req, res) => {
+//     res.json({
+//         message: "Voici un message de l'API"
+//     });
+// });
+
+// const link = "https://www.youtube.com/watch?v=kagoEGKHZvU&list=PLFGcLQodWjBu1Ybi7PoNbyTLaRwvGtyLl&index=3";
+// app.get('/download', async (req, res) => {
+//     console.log("telechargement en cours");
+//     const video = ytdl(link, { quality: 'highestaudio' });
+//     const filePath = "assets/music/audio.mp3";
+
+//     let info = await ytdl.getInfo(link);
+//     let name = info.player_response.videoDetails.title;
+//     let duration = info.player_response.streamingData.formats[0].approxDurationMs;
+    
+//     const response = {
+//         name: name,
+//         duration: duration
+//     }
+
+//     video.pipe(fs.createWriteStream(filePath));
+//     video.on('end', () => {
+//         console.log("Téléchargement terminé");
+//         res.send(response);
+//     });
+// });
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -7,33 +7,12 @@ const port = 5000;
 
 const app = express();
 
-app.get('/post', (req, res) => {
-    res.json({
-        message: "Voici un message de l'API"
-    });
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-const link = "https://www.youtube.com/watch?v=kagoEGKHZvU&list=PLFGcLQodWjBu1Ybi7PoNbyTLaRwvGtyLl&index=3";
-app.get('/download', async (req, res) => {
-    console.log("telechargement en cours");
-    const video = ytdl(link, { quality: 'highestaudio' });
-    const filePath = "assets/music/audio.mp3";
+app.use('/download', require('./routes/download.routes'));
 
-    let info = await ytdl.getInfo(link);
-    let name = info.player_response.videoDetails.title;
-    let duration = info.player_response.streamingData.formats[0].approxDurationMs;
-    
-    const response = {
-        name: name,
-        duration: duration
-    }
 
-    video.pipe(fs.createWriteStream(filePath));
-    video.on('end', () => {
-        console.log("Téléchargement terminé");
-        res.send(response);
-    });
-});
 
 //lancer le serveur
 app.listen(port, () => console.log("Le serveur a démarré au port : " + port));
