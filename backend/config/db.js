b542b5ce0bbc58ffa995f6bc60cf596460c1065c
Anthwoine <Anthwoine49@gmail.com>
2023-12-04 23:09:21 +0100

    ajout de la communication avec la DB SQL

---
+++ backend/config/db.js
@@ -0,0 +1,31 @@
+let mysql = require('mysql');
+
+// local mysql db connection
+let mySqlClient = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "audioplayer_db" 
+});
+
+const request = async () => {
+    let selectQuery = "DESCRIBE music";
+    mySqlClient.query(
+        selectQuery,
+        function (error, result, fields) {
+            if (error) {
+                console.log("Error: ", error);
+                mySqlClient.end();
+            }
+
+            if(result.length > 0) {
+                console.log("Results : \n\n", result);
+            } else {
+                console.log("No results found");
+            }
+            mySqlClient.end();
+        }
+    )
+}
+
+module.exports = request;
